39d22e077ded3afbde61e94ccfa99cb0cc921be0
kgul1 <kacpergul13@gmail.com>
2023-02-04 10:45:24 +0100

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <windows.h>
+using namespace  std;
+
+int main() {
+
+  for(int i=15;i>0;i--){
+      Sleep(1000);
+      cout<<i<<endl;
+  }
+    cout<<"bum";
+
+ return 0;
+}
